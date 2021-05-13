2e0953cb56f63cc61addb961f2964ee98fbef09d
paruqute <parvathychandran.t@gmail.com>
2021-05-13 18:16:08 +0530

    dev branch created

---
+++ lib/Models/Today_Deals.dart
@@ -0,0 +1,29 @@
+class Deals{
+  final String title, img,descrip;
+  final int oldrs,newrs;
+  Deals({this.img,this.title,this.descrip,this.oldrs,this.newrs});
+}
+
+List<Deals> deal=[
+  Deals(
+    title: "Fridge",
+    img: "assets/fridge.jpg",
+    descrip: "Whirlpool 190 L 3 Star \n Direct-Cool Single Door Refrigerator",
+    oldrs: 12000,
+    newrs: 9000
+  ),
+  Deals(
+    title: "LED",
+      img: "assets/fridge.jpg",
+      descrip: "Whirlpool 190 L 3 Star \n Direct-Cool Single Door Refrigerator",
+      oldrs: 12000,
+      newrs: 9000
+  ),
+  Deals(
+    title: "Headset",
+      img: "assets/fridge.jpg",
+      descrip: "Whirlpool 190 L 3 Star \n Direct-Cool Single Door Refrigerator",
+      oldrs: 12000,
+      newrs: 9000
+  )
+];
